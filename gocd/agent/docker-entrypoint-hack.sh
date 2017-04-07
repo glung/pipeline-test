43f53ca2f68fee94480ed30c0d4d1a91d4607ea8
Guillaume Lung <guillaume@soundcloud.com>
2017-04-07 20:15:48 +0200

    Share /var/run/docker.sock easily by enabling agent to run with root.

--- gocd/agent/docker-entrypoint-hack.sh
+++ gocd/agent/docker-entrypoint-hack.sh
@@ -43,10 +43,8 @@ setup_autoregister_properties_file_for_normal_agent() {
 setup_autoregister_properties_file() {
   if [ -n "$GO_EA_SERVER_URL" ]; then
     setup_autoregister_properties_file_for_elastic_agent "$1"
-    try chown go:go $1
   else
     setup_autoregister_properties_file_for_normal_agent "$1"
-    try chown go:go $1
   fi
 }
 
@@ -66,40 +64,33 @@ fi
 # if running go server as root, then initialize directory structure and call ourselves as `go` user
 if [ "$1" = '/go-agent/agent.sh' ]; then
 
-  if [ "$(id -u)" = '0' ]; then
-    server_dirs=(config logs pipelines)
+  server_dirs=(config logs pipelines)
 
-    yell "Creating directories and symlinks to hold GoCD configuration, data, and logs"
+  yell "Creating directories and symlinks to hold GoCD configuration, data, and logs"
 
-    # ensure working dir exist
-    if [ ! -e "${AGENT_WORK_DIR}" ]; then
-      try mkdir -p "${AGENT_WORK_DIR}"
-      try chown go:go "${AGENT_WORK_DIR}"
-    fi
-
-    # ensure proper directory structure in the volume directory
-    if [ ! -e "${VOLUME_DIR}" ]; then
-      try mkdir "${VOLUME_DIR}"
-      try chown go:go "${AGENT_WORK_DIR}"
-    fi
-
-    for each_dir in "${server_dirs[@]}"; do
-      if [ ! -e "${VOLUME_DIR}/${each_dir}" ]; then
-        try mkdir -v "${VOLUME_DIR}/${each_dir}"
-        try chown go:go "${VOLUME_DIR}/${each_dir}"
-      fi
-
-      if [ ! -e "${AGENT_WORK_DIR}/${each_dir}" ]; then
-        try ln -sv "${VOLUME_DIR}/${each_dir}" "${AGENT_WORK_DIR}/${each_dir}"
-        try chown go:go "${AGENT_WORK_DIR}/${each_dir}"
-      fi
-    done
-
-    setup_autoregister_properties_file "${AGENT_WORK_DIR}/config/autoregister.properties"
-    touch "${STDOUT_LOG_FILE}"
-    chown go:go "${STDOUT_LOG_FILE}"
-	  try exec /usr/local/sbin/tini -- /usr/local/sbin/gosu go "$0" "$@" >> ${STDOUT_LOG_FILE} 2>&1
+  # ensure working dir exist
+  if [ ! -e "${AGENT_WORK_DIR}" ]; then
+    try mkdir -p "${AGENT_WORK_DIR}"
   fi
+
+  # ensure proper directory structure in the volume directory
+  if [ ! -e "${VOLUME_DIR}" ]; then
+    try mkdir "${VOLUME_DIR}"
+  fi
+
+  for each_dir in "${server_dirs[@]}"; do
+    if [ ! -e "${VOLUME_DIR}/${each_dir}" ]; then
+      try mkdir -v "${VOLUME_DIR}/${each_dir}"
+    fi
+
+    if [ ! -e "${AGENT_WORK_DIR}/${each_dir}" ]; then
+      try ln -sv "${VOLUME_DIR}/${each_dir}" "${AGENT_WORK_DIR}/${each_dir}"
+    fi
+  done
+
+  setup_autoregister_properties_file "${AGENT_WORK_DIR}/config/autoregister.properties"
+  touch "${STDOUT_LOG_FILE}"
+  try exec /usr/local/sbin/tini -- "$0" "$@" >> ${STDOUT_LOG_FILE} 2>&1
 fi
 
 try exec "$@"
